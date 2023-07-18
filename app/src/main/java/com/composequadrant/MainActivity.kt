9f0204943d6deae4467561125dc331ef26189f4e
ntwigamartin <ntwiga.martin@gmail.com>
2023-07-18 23:04:39 +0300

    added string resources

--- app/src/main/java/com/composequadrant/MainActivity.kt
+++ app/src/main/java/com/composequadrant/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.composequadrant.ui.theme.ComposeQuadrantTheme
 
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    ComposeQuadrant("Android")
                 }
             }
         }
@@ -30,7 +31,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun ComposeQuadrant(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
@@ -39,8 +40,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ComposeQuadrantPreview() {
     ComposeQuadrantTheme {
-        Greeting("Android")
+        ComposeQuadrant("Android")
     }
 }
